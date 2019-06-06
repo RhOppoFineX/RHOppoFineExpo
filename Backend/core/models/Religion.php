f1f921ae16c5966110abb5fe2644ee1f32dbce07
Rodrigo Barillas <50836471+RodrigoBarillas@users.noreply.github.com>
2019-06-05 21:47:26 -0600

    cambios submit

---
+++ Backend/core/models/Religion.php
@@ -0,0 +1,79 @@
+<?php
+class Religion extends Validator    
+{
+        private $Id = null;
+        private $Religion = null;
+
+        public function setId($Id)
+        {
+            if($this->validateId($Id))
+            {
+                $this->Id = $Id;
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
+        public function getId()
+        {
+            return $this->Id;
+        }
+
+        public function setReligion($Religion)
+        {
+            if($this->validateAlphanumeric($Religion, 1, 50)){
+                $this->Religion = $Religion;
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        public function getReligion()
+        {
+            return $this->Religion;
+        }
+
+        public function insertReligion()
+        {
+            $sql = "INSERT INTO Religion (Religion)
+            VALUES (?)";            
+            $parametros = array($this->Religion);
+            return Database::executeRow($sql, $parametros);
+        }
+
+        public function updateReligion()
+        {
+            $sql = "UPDATE Religion SET Religion = ? WHERE Id_religion = ?";
+            $parametros = array($this->Religion, $this->Id);
+            return Database::executeRow($sql, $parametros);
+        }
+
+        public function deleteReligion()
+        {
+            $sql = "DELETE FROM Religion WHERE Id_religion = ?";
+            $parametros = array($this->Id);
+            return Database::executeRow($sql, $parametros);
+        }
+
+        public function selectReligion()
+        {
+            $sql = "SELECT Id_religion, Religion FROM Religion 
+            ORDER BY Religion";
+            $parametros = array(null);
+            return Databse::getRows($parametros);
+        }
+
+        public function buscarReligion($valor)
+        {
+            $sql = "SELECT * FROM Religion WHERE Religion LIKE ? ORDER BY Religion";
+            $parametros = array("%$valor%");
+            return Database::getRows($sql, $parametros);
+        }
+
+                        
+
+}
+?>
---
+++ Backend/core/models/datoIdentificacion.php
@@ -0,0 +1,20 @@
+<?php
+
+class datosIdentificacion extends Validator
+{
+    private $Id = null;
+    private $Documento = null;
+    private $Residencia = null;
+    private $lugarExpedicion = null;
+    private $Fecha = null;
+    private $Profesion = null;
+    private $IdEstadoCivil = null;
+    private $FechaExpiracion = null;
+    private $Num_isss = null;
+    private $AFP = null;
+    private $NUP = null;
+    private $TipoDocumento = null;
+    private $EstadoCivil = null;
+}
+
+?>
