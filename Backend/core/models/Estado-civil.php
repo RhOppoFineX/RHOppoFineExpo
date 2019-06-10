1c73bb70b2243e7acafbb64288fa89bd437ac1bf
Cristian-EA <cristianeduardoayala@gmail.com>
2019-06-10 10:36:33 -0600

    CRUD Estado Civil

---
+++ Backend/core/models/Estado-civil.php
@@ -0,0 +1,86 @@
+<?php
+class Estado extends Validator    
+{       
+        //Campos segun la base de datos siempre con sus metodos set() y get()
+        private $Id = null;
+        private $Estado_civil = null;
+
+        public function setId($Id)
+        {
+            if($this->validateId($Id))
+            {
+                $this->Id = $Id;
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
+        public function getId()
+        {
+            return $this->Id;
+        }
+
+        public function setEstado($Estado_civil)
+        {
+            if($this->validateAlphanumeric($Estado_civil, 1, 25)){ 
+                $this->Estado_civil = $Estado_civil;
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        public function getEstado()
+        {
+            return $this->Estado_civil;
+        }
+
+        public function insertEstado()
+        {
+            $sql = "INSERT INTO Estado_civil (Estado_civil)
+            VALUES (?)";            
+            $parametros = array($this->Estado_civil);
+            return Database::executeRow($sql, $parametros);
+        }
+
+        public function updateEstado()
+        {
+            $sql = "UPDATE Estado_civil SET Estado_civil = ? WHERE Id_estado_civil = ?";
+            $parametros = array($this->Estado_civil, $this->Id);
+            return Database::executeRow($sql, $parametros);
+        }
+
+        public function deleteEstado()
+        {
+            $sql = "DELETE FROM Estado_civil WHERE Id_estado_civil = ?";
+            $parametros = array($this->Id);
+            return Database::executeRow($sql, $parametros);
+        }
+
+        public function selectEstado()
+        {
+            $sql = "SELECT Id_estado_civil, Estado_civil FROM estado_civil 
+            ORDER BY Estado_civil";
+            $parametros = array(null);
+            return Database::getRows($sql, $parametros);
+        }
+
+        public function buscarEstado($valor)
+        {
+            $sql = "SELECT * FROM estado_civil WHERE Estado_civil LIKE ? ORDER BY Estado_civil";
+            $parametros = array("%$valor%");
+            return Database::getRows($sql, $parametros);
+        }
+
+        //Extrare los datos de la base hacia el actualizarModal esta es su mision
+        public function getEstadoModal()
+        {
+            $sql = "SELECT * FROM estado_civil WHERE Id_estado_civil = ?";
+            $parametros = array($this->Id);
+            return Database::getRow($sql, $parametros);
+        }                       
+
+}
+?>
--- Frontend/estado-civil.php
+++ Frontend/estado-civil.php
@@ -1,28 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
 
-    <title>Tabla | Datos estado civil </title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<!-- VENDOR CSS -->
-	<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
-	<!-- MAIN CSS -->
-	<link rel="stylesheet" href="assets/css/main.css">
-	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
-	<link rel="stylesheet" href="assets/css/demo.css">
-	<!-- GOOGLE FONTS -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
-	<!-- ICONS -->
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+    <title>Tabla | Datos-Estado civil </title>
+	
+	<?php
+		require_once '../backend/core/helpers/css.php';	//Hojas de estilos CSS
+	?>
+
 </head>
 <body>
 
@@ -41,7 +30,8 @@
                                 <!-- TABLE STRIPED -->
                                 <div class="panel">
 									<div class="panel-heading">
-										<h3 class="panel-title">Datos estado civil</h3>
+										<!--<h3 class="panel-title">Datos estado civil</h3>-->
+										<a type="button" class="btn btn-primary btn-lg" onclick="modalCreate()">Agregar nuevo registro <span class="lnr lnr-file-add"></span></a>	
 									</div>
 									<div class="panel-body no-padding">
 										<table class="table table-striped">
@@ -51,25 +41,13 @@
                                                     <th>Estado civil</th>
 												</tr>
 											</thead>
-											<tbody>
+											<tbody id="tabla-estado">
 												<tr>
 													<td>1</td>
-													<td>---</td>									
-													<td><button type="submit" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#Modal1">Modificar</button></td>
-													<td><button type="submit" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal">Deshabilitar</button></td>
-												</tr>
-												<tr>
-													<td>2</td>	
-													<td>---</td>
-													<td><button type="submit" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#Modal1">Modificar</button></td>
-													<td><button type="submit" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal">Deshabilitar</button></td>							
-												</tr>
-												<tr>
-													<td>3</td>
-													<td>---</td>
-													<td><button type="submit" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#Modal1">Modificar</button></td>
-													<td><button type="submit" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal">Deshabilitar</button></td>	
-												</tr>
+													<td>Viudo</td>									
+													<td><a class="btn btn-warning btn-sm" data-toggle="modal" onclick="actualizarModal()">Modificar</a></td>
+													<td><a class="btn btn-danger btn-sm" data-toggle="modal" onclick="confirmDelete()">Deshabilitar</a></td>
+													</tr>
 											</tbody>
 										</table>
 									</div>
@@ -82,8 +60,8 @@
 				</div><!-- END MAIN --><!--Fin de la Table-->
 			</div>	<!--Wrapper Fin-->
 
-			<!-- Modal 1-->
-<div class="modal fade bd-modificar-modal-xl" id="Modal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+			<!-- Inicio Modal Modificar-->
+<div class="modal fade bd-modificar-modal-xl" id="modificar-estadoCivil" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-scrollable" role="document">
 		<div class="modal-content">
 		<div class="modal-header">
@@ -94,32 +72,29 @@
 		</div>
 		<div class="modal-body">
 			<div class="container-fluid">
-					<form method="POST" id="registro-estadocivil">
+					<form method="POST" id="actualizar-estado">
 							<div class="form-row">
 									<div class="form-group col-md-12">
+			<!--nuevo inpunt es invisible--><input type="hidden" id="Id_estado-civil" name="Id_estado-civil">
 											<label for="EstadoCivil">Estado civil</label>
-											<input type="text" class="form-control" id="EstadoCivil" aria-describedby="puestoHelp" placeholder="estado civil...">
+											<input type="text" class="form-control" id="EstadoCivil" aria-describedby="puestoHelp" placeholder="Estado civil" name="EstadoCivil">
 											<small id="puestoHelp" class="form-text text-muted"></small>
-									</div>
+								</div>
 							</div>
-							</div>
-						</form>
-			</div>			  
+				</div>			  
 		</div>
 		<div class="modal-footer">
 			<button type="submit" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-			<button type="submit" class="btn btn-primary" data-dismiss="modal">Modificar</button>
+			<button type="submit" class="btn btn-primary">Modificar</button>
+			</form>
 		</div>
 		</div>
-	</div>  <!--Fin del modal 1-->
-
-		<!--Modal Deshabilitar-->
+	</div>  <!--Fin del modal modificar-->
 
 	<!-- Button trigger modal -->
-
 	
-	<!-- Modal -->
-	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<!-- Inicio Modal Deshabilitar -->
+	<div class="modal fade" id="eliminar-estado-civil" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
@@ -141,133 +116,47 @@
 
 	<!--Fin del modal Deshabilitar-->	
 
+	<!--Inicio modal insertar -->
 
+	<div class="modal fade bd-modificar-modal-xl" id="Insertar-estadoCivil" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+		<div class="modal-header">
+			<h5 class="modal-title" id="exampleModalScrollableTitle">Datos de la Religión</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="modal-body">
+			<div class="container-fluid">
+					<form method="post" id="estadoCivil-Insertar">
+							<div class="form-group col-md-12">							
+									<label for="EstadoCivil">Estado Civil</label>
+									<input type="text" class="form-control" id="EstadoCivil" aria-describedby="estadoHelp" placeholder="Estado Civil" name="Estado Civil">
+									<small id="estadoHelp" class="form-text text-muted"></small>
+							</div>
+						
+					</div>											
+			</div>			  
+		</div>
+		<div class="modal-footer">
+			<button type="submit" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			<button type="submit" class="btn btn-primary">Agregar</button><!--Quitarle el data-dismiss-->
+			</form>
+		</div>
+		</div>
+	</div>  <!--Fin del modal Insertar-->
 
+<!--Scripts necesarios siempre-->
+<?php
+		require_once '../Backend/core/helpers/scripts.php';
+	?>
+	<!--Scripts para los cruds-->
 
-
-    <script src="assets/vendor/jquery/jquery.min.js"></script>
-	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-	<script src="assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-	<script src="assets/vendor/chartist/js/chartist.min.js"></script>
-	<script src="assets/scripts/klorofil-common.js"></script>
-	<script>
-	// $(function() {
-	// 	var data, options;
-
-	// 	// headline charts
-	// 	data = {
-	// 		labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-	// 		series: [
-	// 			[23, 29, 24, 40, 25, 24, 35],
-	// 			[14, 25, 18, 34, 29, 38, 44],
-	// 		]
-	// 	};
-
-	// 	options = {
-	// 		height: 300,
-	// 		showArea: true,
-	// 		showLine: false,
-	// 		showPoint: false,
-	// 		fullWidth: true,
-	// 		axisX: {
-	// 			showGrid: false
-	// 		},
-	// 		lineSmooth: false,
-	// 	};
-
-	// 	new Chartist.Line('#headline-chart', data, options);
-
-
-	// 	// visits trend charts
-	// 	data = {
-	// 		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-	// 		series: [{
-	// 			name: 'series-real',
-	// 			data: [200, 380, 350, 320, 410, 450, 570, 400, 555, 620, 750, 900],
-	// 		}, {
-	// 			name: 'series-projection',
-	// 			data: [240, 350, 360, 380, 400, 450, 480, 523, 555, 600, 700, 800],
-	// 		}]
-	// 	};
-
-	// 	options = {
-	// 		fullWidth: true,
-	// 		lineSmooth: false,
-	// 		height: "270px",
-	// 		low: 0,
-	// 		high: 'auto',
-	// 		series: {
-	// 			'series-projection': {
-	// 				showArea: true,
-	// 				showPoint: false,
-	// 				showLine: false
-	// 			},
-	// 		},
-	// 		axisX: {
-	// 			showGrid: false,
-
-	// 		},
-	// 		axisY: {
-	// 			showGrid: false,
-	// 			onlyInteger: true,
-	// 			offset: 0,
-	// 		},
-	// 		chartPadding: {
-	// 			left: 20,
-	// 			right: 20
-	// 		}
-	// 	};
-
-	// 	new Chartist.Line('#visits-trends-chart', data, options);
-
-
-	// 	// visits chart
-	// 	data = {
-	// 		labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-	// 		series: [
-	// 			[6384, 6342, 5437, 2764, 3958, 5068, 7654]
-	// 		]
-	// 	};
-
-	// 	options = {
-	// 		height: 300,
-	// 		axisX: {
-	// 			showGrid: false
-	// 		},
-	// 	};
-
-	// 	new Chartist.Bar('#visits-chart', data, options);
-
-
-	// 	// real-time pie chart
-	// 	var sysLoad = $('#system-load').easyPieChart({
-	// 		size: 130,
-	// 		barColor: function(percent) {
-	// 			return "rgb(" + Math.round(200 * percent / 100) + ", " + Math.round(200 * (1.1 - percent / 100)) + ", 0)";
-	// 		},
-	// 		trackColor: 'rgba(245, 245, 245, 0.8)',
-	// 		scaleColor: false,
-	// 		lineWidth: 5,
-	// 		lineCap: "square",
-	// 		animate: 800
-	// 	});
-
-	// 	var updateInterval = 3000; // in milliseconds
-
-	// 	setInterval(function() {
-	// 		var randomVal;
-	// 		randomVal = getRandomInt(0, 100);
-
-	// 		sysLoad.data('easyPieChart').update(randomVal);
-	// 		sysLoad.find('.percent').text(randomVal);
-	// 	}, updateInterval);
-
-	// 	function getRandomInt(min, max) {
-	// 		return Math.floor(Math.random() * (max - min + 1)) + min;
-	// 	}
-
-	// });
-	</script>
+	<script src="../Backend/libraries/sweetalert.min.js"></script><!--Libreria para los mensajes de confirmacion-->
+	<script src="../Backend/core/helpers/validator.js"></script>
+	<script src="../Backend/core/helpers/components.js"></script>
+	<script src="../Backend/core/controllers/estadoCivil.js"></script>
+	<!--Los primeros Tres scripts siempre son los mismos el que cambia son los controladores-->
 </body>
 </html>
