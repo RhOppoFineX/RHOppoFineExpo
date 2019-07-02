57b78d4d767630caf80b83dc89a1e825226d8231
Diego Arias <d.arias2000.eh@gmail.com>
2019-07-01 21:21:45 -0600

    validar campos vacios

--- Frontend/register.php
+++ Frontend/register.php
@@ -20,33 +20,33 @@
 							<form method="post" id="register-usuario">								
 										<div class="form-group col-md-4">
 											<label for="Nombres">Nombres</label>
-											<input type="Text" class="form-control" id="Nombres" placeholder="Nombres" name="Nombres">
+											<input type="Text" class="form-control" id="Nombres" placeholder="Nombres" name="Nombres" required>
 										</div>
 										<div class="form-group col-md-4">
 											<label for="Apellidos">Apellidos</label>
-											<input type="Text" class="form-control" id="Apellidos" placeholder="Apellidos" name="Apellidos">
+											<input type="Text" class="form-control" id="Apellidos" placeholder="Apellidos" name="Apellidos" required>
 										</div>
 										<div class="form-group col-md-4">
 											<label for="Correo">Correo electonico</label>
-											<input type="email" class="form-control" id="Correo" aria-describedby="emailHelp" placeholder="Correo Institucional" name="Correo">
+											<input type="email" class="form-control" id="Correo" aria-describedby="emailHelp" placeholder="Correo Institucional" name="Correo" required>
 											<small id="emailHelp" class="form-text text-muted"></small>
 										</div>
 			
 										<div class="form-group col-md-4">
 											<label for="userName">User Name</label>
-											<input type="text" class="form-control" id="userName" aria-describedby="emailHelp" placeholder="userName Institucional" name="userName">
+											<input type="text" class="form-control" id="userName" aria-describedby="emailHelp" placeholder="userName Institucional" name="userName" required>
 											<small id="emailHelp" class="form-text text-muted"></small>
 										</div>
 			
 										<div class="form-group col-md-4">
 											<label for="Contraseña">Contraseña</label>
-											<input type="password" class="form-control" id="Contraseña" aria-describedby="emailHelp" placeholder="Contraseña Institucional" name="Contraseña">
+											<input type="password" class="form-control" id="Contraseña" aria-describedby="emailHelp" placeholder="Contraseña Institucional" name="Contraseña" required>
 											<small id="emailHelp" class="form-text text-muted"></small>
 										</div>
 			
 										<div class="form-group col-md-4">
 											<label for="ContraseñaDos">Repetir Contraseña</label>
-											<input type="password" class="form-control" id="ContraseñaDos" aria-describedby="emailHelp" placeholder="ContraseñaDos Institucional" name="ContraseñaDos">
+											<input type="password" class="form-control" id="ContraseñaDos" aria-describedby="emailHelp" placeholder="ContraseñaDos Institucional" name="ContraseñaDos" required>
 											<small id="emailHelp" class="form-text text-muted"></small>
 										</div>
 
