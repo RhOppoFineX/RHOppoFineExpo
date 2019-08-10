cf87f98ce8458e14d02621c8cde52b1917b67a86
Diego Arias <d.arias2000.eh@gmail.com>
2019-08-10 17:39:57 -0600

    Commit Rodrigo grafica de tipos de usuario

--- Backend/core/controllers/charts.js
+++ Backend/core/controllers/charts.js
@@ -22,14 +22,16 @@ function graficoEjemplo()
                 sweetAlert(4, result.exception, null);
             }
 
-            let id = [];
-                let cantidad = [];
-                result.dataset.forEach(function (rows) {
-                    id.push(rows.IdProducto);
-                    cantidad.push(rows.nombre);
+                let Usuario = [];
+                let Cantidad = [];
+                
+                result.dataset.forEach(fila => {
+                    Usuario.push(fila.Usuario);//fila.nombre_que_le pusieron_despues_del_AS_en_la_consulta
+                    Cantidad.push(fila.Cantidad);//fila.nombre_que_le pusieron_despues_del_AS_en_la_consulta
                 });
+
             //grafico1 es el ID de la etiqueta canvas en html
-            graficoBarra('grafico1', cantidad, id, 'Cantidad de producto', 'Grafico', 'bar');//el ultimo parametro es el tipo de grafica bar para barras y pie para circular
+            barGraph('grafico1', Cantidad, Usuario, 'Cantidad de producto', 'Grafico', 'bar');//el ultimo parametro es el tipo de grafica bar para barras y pie para circular o pastel
             
         } else {
             console.log(response);
--- Backend/core/models/graficos.php
+++ Backend/core/models/graficos.php
@@ -6,7 +6,7 @@ class graficos extends Validator
     //Ejemplo nada más 
     public function ejemplo()
     {
-        $sql = 'SELECT id_garantia, garantia FROM garantia';
+        $sql = 'SELECT count(U.Id_usuario) as Usuario, Tipo_usuario as Cantidad FROM Usuario as U INNER JOIN Tipo_usuario as T ON U.Id_tipo_usuario = T.Id_tipo_usuario GROUP BY U.Id_tipo_usuario';
 		$params = array(null);
 		return Database::getRows($sql, $params);
     }
