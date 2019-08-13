832d6b791270e7cd37bb78d1b4aedc75143c3bee
Rodrigo Barillas <50836471+RodrigoBarillas@users.noreply.github.com>
2019-08-12 19:31:19 -0600

    Crud area laboral

---
+++ Backend/core/controllers/area-laboral.js
@@ -0,0 +1,201 @@
+$(document).ready(function()
+{
+    showTable();
+})
+
+// Constante para establecer la ruta y parámetros de comunicación con la apiidioma
+const apiidioma = '../../RHOppoFineExpo/Backend/core/api/idioma.php?action=';
+
+const tablaPadre = '../../RHOppoFineExpo/Backend/core/api/nivel-idioma.php?action=read';
+
+// Función para llenar tabla con los datos de los registros
+function fillTable(rows)
+{
+    let content = '';
+    // Se recorren las filas para armar el cuerpo de la tabla y se utiliza comilla invertida para escapar los caracteres especiales
+    rows.forEach(function(row){
+        content += `
+            <tr>                
+                <td>${row.Idioma}</td>
+                <td>${row.Nivel}</td>
+                <td><a class="btn btn-warning btn-sm" onclick="actualizarModal(${row.Id_idioma})">Modificar</a></td>
+				<td><a class="btn btn-danger btn-sm" onclick="confirmDelete('${apiidioma}', ${row.Id_idioma}, null)">Deshabilitar</a></td> 
+            </tr>
+        `;
+    });
+    $('#tabla-idioma').html(content);       
+}
+
+// Función para obtener y mostrar los registros disponibles
+function showTable()
+{
+    $.ajax({
+        url: apiidioma + 'read',
+        type: 'post',
+        data: null,
+        datatype: 'json'
+    })
+    .done(function(response){
+        // Se verifica si la respuesta de la apiidioma es una cadena JSON, sino se muestra el resultado en consola
+        if (isJSONString(response)) {
+            const result = JSON.parse(response);
+            // Se comprueba si el resultado es satisfactorio, sino se muestra la excepción
+            if (!result.status) {
+                sweetAlert(4, result.exception, null);
+            }
+            fillTable(result.dataset);
+        } else {
+            console.log(response);
+        }
+    })
+    .fail(function(jqXHR){
+        // Se muestran en consola los posibles errores de la solicitud AJAX
+        console.log('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+    });
+}
+
+// Función para mostrar los resultados de una búsqueda
+$('#form-search').submit(function()
+{
+    event.preventDefault();
+    $.ajax({
+        url: apiidioma + 'search',
+        type: 'post',
+        data: $('#form-search').serialize(),
+        datatype: 'json'
+    })
+    .done(function(response){
+        // Se verifica si la respuesta de la apiidioma es una cadena JSON, sino se muestra el resultado en consola
+        if (isJSONString(response)) {
+            const result = JSON.parse(response);
+            // Se comprueba si el resultado es satisfactorio, sino se muestra la excepción
+            if (result.status) {
+                fillTable(result.dataset);
+                sweetAlert(1, result.message, null);
+            } else {
+                sweetAlert(3, result.exception, null);
+            }
+        } else {
+            console.log(response);
+        }
+    })
+    .fail(function(jqXHR){
+        // Se muestran en consola los posibles errores de la solicitud AJAX
+        console.log('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+    });
+})
+
+// Función para mostrar formulario insertar en blanco
+function modalCreate()
+{
+    $('#agregaridioma')[0].reset();//Id del formulario
+    fillSelect(tablaPadre, 'Nivel-A', null);//llenar el combo
+    //Tipos-A es el Id del combobox
+    $('#idiomaAgregar').modal('show');//Id del modal
+}
+
+// Función para crear un nuevo registro
+$('#agregaridioma').submit(function()
+{
+    event.preventDefault();
+    $.ajax({
+        url: apiidioma + 'create',
+        type: 'post',
+        data: new FormData($('#agregaridioma')[0]),
+        datatype: 'json',
+        cache: false,
+        contentType: false,
+        processData: false
+    })
+    .done(function(response){
+        // Se verifica si la respuesta de la apiidioma es una cadena JSON, sino se muestra el resultado en consola
+        if (isJSONString(response)) {
+            const result = JSON.parse(response);
+            // Se comprueba si el resultado es satisfactorio, sino se muestra la excepción
+            if (result.status) {               
+                $('#idiomaAgregar').modal('hide');
+                showTable();
+                sweetAlert(1, result.message, null);
+            } else {
+                sweetAlert(2, result.exception, null);
+            }
+        } else {
+            console.log(response);
+        }
+    })
+    .fail(function(jqXHR){
+        // Se muestran en consola los posibles errores de la solicitud AJAX
+        console.log('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+    });
+})
+
+// Función para mostrar formulario con registro a modificar
+function actualizarModal(id)
+{
+    $.ajax({
+        url: apiidioma + 'get',
+        type: 'post',
+        data:{
+            Id_idioma: id
+        },
+        datatype: 'json'
+    })
+    .done(function(response){
+        // Se verifica si la respuesta de la apiidioma es una cadena JSON, sino se muestra el resultado consola
+        if (isJSONString(response)) {
+            const result = JSON.parse(response);
+            // Se comprueba si el resultado es satisfactorio para mostrar los valores en el formulario, sino se muestra la excepción
+            if (result.status) {
+                $('#Id_idioma').val(result.dataset.Id_idioma);
+                $('#idioma').val(result.dataset.Idioma);
+                fillSelect(tablaPadre, 'nivel', result.dataset.Id_nivel_idioma);           
+                $('#idiomaModificar').modal('show');   
+            } else {
+                sweetAlert(2, result.exception, null);
+            }
+        } else {
+            console.log(response);
+        }
+    })
+    .fail(function(jqXHR){
+        // Se muestran en consola los posibles errores de la solicitud AJAX
+        console.log('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+    });
+}
+
+// Función para modificar un registro seleccionado previamente
+//Id del formulario
+$('#modificaridioma').submit(function()
+{
+    event.preventDefault();
+    $.ajax({
+        url: apiidioma + 'update',
+        type: 'post',
+        data: new FormData($('#modificaridioma')[0]),
+        datatype: 'json',
+        cache: false,
+        contentType: false,
+        processData: false
+    })
+    .done(function(response){
+        // Se verifica si la respuesta de la apiidioma es una cadena JSON, sino se muestra el resultado en consola
+        if (isJSONString(response)) {
+            const result = JSON.parse(response);
+            // Se comprueba si el resultado es satisfactorio, sino se muestra la excepción
+            if (result.status) {
+                $('#idiomaModificar').modal('hide');
+                showTable();
+                sweetAlert(1, result.message, null);         
+                
+            } else {
+                sweetAlert(2, result.exception, null);                
+            }
+        } else {
+            console.log(response);
+        }
+    })
+    .fail(function(jqXHR){
+        // Se muestran en consola los posibles errores de la solicitud AJAX
+        console.log('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+    });
+})
---
+++ Frontend/area-laboral.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+
+    <title>Tabla | Datos-Area laboral </title>
+	
+	<?php
+		require_once '../backend/core/helpers/css.php';	//Hojas de estilos CSS
+	?>
+</head>
+<body>
+
+	<div id="wrapper">
+			<?php
+				require_once '../backend/core/helpers/menu.php';	
+			?>
+				<!--Incio de la Table-->
+				<div class="main">
+					<div class="main-content">
+						<div class="container-fluid">
+							<h3 class="page-title">Datos de las areás laborales</h3>
+							<div class="col-md-12">
+                                <!-- TABLE STRIPED -->
+                                <div class="panel">
+									<div class="panel-heading">
+										<!--Boton Agregar-->												
+										<a type="button" class="btn btn-primary btn-lg" onclick="modalCreate()">Agregar nuevo registro <span class="lnr lnr-file-add"></span></a>										
+									</div>
+									<div class="panel-body no-padding">
+										<table class="table table-striped" id="Tablaidioma">
+											<thead>
+												<tr>													
+                                                    <th>#</th>
+                                                    <th>Area</th>
+                                                    <th>Puesto</th>
+                                                    <th>Sueldo plaza</th>
+                                                    <th>Fecha ingreso</th>
+                                                    <th>Inicio contrato</th>
+                                                    <th>Fin contrato</th>
+                                                    <th>Horas laborales</th>
+												</tr>
+											</thead>
+											<tbody id="tabla-laboral">
+												
+											</tbody>
+										</table>
+									</div>
+								</div>                            
+								<!-- END TABLE STRIPED -->
+							</div>														
+						</div>
+					</div><!-- END MAIN CONTENT -->
+				</div><!-- END MAIN --><!--Fin de la Table-->
+			</div>	<!--Wrapper Fin-->
+
+<!-- Modal Modificar idioma-->
+<div class="modal fade bd-modificar-modal-xl" id="idiomaModificar" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+		<div class="modal-header">
+			<h5 class="modal-title" id="ModalTitulo">Datos de idioma</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="modal-body">
+			<div class="container-fluid">
+					<form method="post" id="modificaridioma">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="idioma">idioma</label>
+                                <input type="Text" class="form-control" id="idioma" placeholder="idioma" name="idioma">
+                            </div>
+							<div class="form-group col-md-4">
+                                <label for="nivel">Tipo idioma</label>
+                                <select id="nivel" class="form-control" name="nivel">
+                                        
+                                </select>                               
+                            </div>
+				<!--Input invisible-->		<input type="hidden" id="Id_idioma" name="Id_idioma">
+                        </div>                            
+				</div>					
+			</div>			  
+		</div>
+		<div class="modal-footer">
+			<button type="submit" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			<button type="submit" class="btn btn-primary">Modficar</button>
+			</form>	
+		</div>
+		</div>
+    </div>  <!--Fin del modal modificar idioma-->
+
+
+	<!-- Modal agregar idioma-->
+<div class="modal fade bd-modificar-modal-xl" id="idiomaAgregar" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+		<div class="modal-header">
+			<h5 class="modal-title" id="ModalTitulo">Datos de los idioma</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+			<div class="modal-body">
+					<div class="container-fluid">
+						<form method="post" id="agregaridioma">
+							<div class="form-row">
+								<div class="form-group col-md-6">
+									<label for="Idioma-A">Idioma</label>
+									<input type="Text" class="form-control" id="Idioma-A" placeholder="Idioma" name="Idioma-A">
+								</div>
+								<div class="form-group col-md-4">
+									<label for="Nivel-A">Tipo de Nivel</label>
+									<select id="Nivel-A" name="Nivel-A" class="form-control">
+									</select>									
+									
+								</div>						
+							</div>
+						</div>                            					
+				</div>			  
+		</div>
+		<div class="modal-footer">
+			<button type="submit" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+			<button type="submit" class="btn btn-primary">Agregar</button>
+			</form>			
+		</div>	
+	</div>
+</div>  <!--Fin del modal agregar idioma-->
+
+
+<!--Modals para editar perfil-->
+<?php
+	require_once '../Backend/core/helpers/perfil.php';
+?>
+
+<!--Scripts necesarios siempre-->
+	<?php
+		require_once '../Backend/core/helpers/scripts.php';
+	?>
+	<!--Scripts para los cruds-->	
+	<script src="../Backend/core/controllers/idioma.js"></script>	<!--Solo dejar el script del controlador-->
+	<!--Los primeros scripts siempre son los mismos el que cambia es el controlador-->
+	
+</body>
+</html>
