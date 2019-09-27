3c1a05de81da2599543f5505a73d8b535fe92c94
Cristian-EA <cristianeduardoayala@gmail.com>
2019-09-27 08:39:30 -0600

    Cambios

---
+++ Backend/core/api/datos-familiares.php
@@ -0,0 +1,179 @@
+<?php 
+require_once '../helpers/database.php';
+require_once '../helpers/validator.php';
+require_once '../models/datos-familiares.php';
+
+if(isset($_GET['action'])){
+    session_start();
+    $Datos = new DatosFamiliares();
+    $result = array('status' => false, 'message' => null, 'exception' => null);
+
+    if(isset($_SESSION['Id_usuario'])){
+
+        switch($_GET['action']){
+            case 'read':
+                if($result['dataset'] = $Datos->readDatosFamiliares()){
+                    $result['status'] = true;                               
+                } else {
+                    $result['exception'] = 'No hay Datos registrados';
+                }                
+            break;
+
+            case 'create':
+                $_POST = $Datos->validateForm($_POST);
+                
+                    if($Datos->setNum_documento($_POST['NumeroDocumento'])){
+                        if($Datos->setResidencia($_POST['Direccion'])){
+                            if($Datos->setLugar_expedicion($_POST['LugarExpedicion'])){
+                                if($Datos->setFecha_expedicion($_POST['FechaExpedicion'])){
+                                    if($Datos->setProfesion($_POST['Profesion'])){
+                                        if($Datos->setId_estado_civil($_POST['Estado'])){
+                                            if($Datos->setFecha_expiracion($_POST['FechaExpiracion'])){
+                                                if($Datos->setNumero_ISSS($_POST['NumeroISSS'])){
+                                                    if($Datos->setAFP($_POST['AFP'])){
+                                                        if($Datos->setNUP($_POST['NUP'])){            
+                                                            if($Datos->setDUI(1)){//ver
+                                                                if($Datos->setId_colaborador($_POST['Colaborador'])){
+                                                                    if($Datos->createDatos()){
+                                                                            $result['status'] = true;
+                                                                            $result['message'] = 'Datos personales insertados continue con los datos de identificacion';
+                                                                            } else {
+                                                                                $result['exception'] = 'Fecha invalida';
+                                                                            }
+                                                                        } else {
+                                                                            $result['exception'] = 'valor invalido';
+                                                                        }                                                                   
+                                                                } else {
+                                                                    $result['exception'] = 'Tipo invalido';
+                                                                }
+                                                            } else {
+                                                                $result['exception'] = 'NUP incorrecto';
+                                                            }
+                                                        } else {
+                                                            $result['exception'] = 'AFP incorrecto';
+                                                        }
+                                                    } else {
+                                                        $result['exception'] = 'ISSS incorrecta';
+                                                    }
+                                                } else {
+                                                    $result['exception'] = 'Fecha de Expiracion incorrecto';
+                                                }
+                                            } else {
+                                                $result['exception'] = 'Estado incorrecto';
+                                            }
+                                        } else {
+                                            $result['exception'] = 'Profesion incorrecto';
+                                        }
+                                    } else {
+                                        $result['exception'] = 'fecha de expedicion incorrecta';
+                                    }
+                                } else {
+                                    $result['exception'] = 'Lugar de expedicion incorrecto';
+                                }
+                            } else {
+                                $result['exception'] = 'Direccion incorrectos';
+                            }
+                        } else {
+                            $result['excpetion'] = 'Numero de documento inconrrecto';
+                        }             
+
+            break;
+
+            case 'get':
+                if($Datos->setId($_POST['Id_Datos'])){
+                    if($result['dataset'] = $Datos->getDatos()){
+                        $result['status'] = true;
+                    } else {
+                        $result['exception'] = 'Datos Inexistente';
+                    }
+                } else {
+                    $result['exception'] = 'Datos Incorrecto';
+                }
+            break;
+
+            case 'update':
+                $_POST = $Datos->validateForm($_POST);   
+
+                if($colaborador->setId($_POST['Id_colaborador_up'])){
+                    if($colaborador->getColaborador()){
+                        if($colaborador->setTelefono_casa($_POST['Telefono_casa_up'])){
+                            if($colaborador->setTelefono_celular($_POST['Telefono_celular_up'])){
+                                if($colaborador->setCorreo_institucional($_POST['Correo_up'])){
+                                    if($colaborador->setDireccion($_POST['Direccion_up'])){
+                                        if($colaborador->setId_religion($_POST['Religion_up'])){
+                                            if($colaborador->setId_municipio($_POST['Municipio_up'])){
+                                                if($colaborador->setNIP($_POST['NIP_up'])){
+                                                    if($colaborador->setNivel($_POST['Nivel_up'])){
+                                                        if($colaborador->setEstudiando($_POST['Estudiando_up'])){
+                                                            if($colaborador->updateColaborador()){
+                                                                $result['status'] = true;
+                                                                $result['message'] = 'Datos personales actualizados';
+                                                            } else {
+                                                                $result['exception'] = 'Operación fallida';
+                                                            }
+                                                        } else {
+                                                            $result['exception'] = 'Valor incorrecto';
+                                                        }
+                                                    } else {
+                                                        $result['exception'] = 'Nivel invalido';
+                                                    }
+                                                } else {
+                                                    $result['exception'] = 'NIP incorrecto';
+                                                }
+                                            } else {
+                                                $result['exception'] = 'Municipio Incorrecto';
+                                            }
+                                        } else {
+                                            $result['exception'] = 'Religion no valida';
+                                        }
+                                    } else {
+                                        $result['exception'] = 'Direccion no valida';
+                                    }
+                                } else {
+                                    $result['exception'] = 'Correo no valido';
+                                }
+                            } else {
+                                $result['exception'] = 'Número invalido';
+                            }
+                        }  else {
+                            $result['exception'] = 'Número invalido';
+                        }                      
+                    } else {
+                        $result['exception'] = 'Colaborador inexistente';
+                    }
+                } else {
+                    $result['exception'] = 'Colaborador incorrecto';
+                }
+            break;            
+
+            case 'disable':
+                if($Datos->setId($_POST['identifier'])){
+                    if($Datos->setEstado(0)){
+                        if($Datos->getDatos()){
+                            if($Datos->disableDatos()){
+                                $result['status'] = true;
+                                $result['message'] = 'Datos de colabirador Deshablitado';
+                            } else {
+                                $result['exception'] = 'Operación fallida';
+                            }
+                        } else {
+                            $result['exception'] = 'Colaborador Inexistente';
+                        }
+                    } else {
+                        $result['exception'] = 'Estado Incorrecto';
+                    }
+                } else {
+                    $result['exception'] = 'Colaborador Incorrecto';
+                }
+            break;
+            
+        }
+
+        print(json_encode($result));
+
+    } else {
+        exit('Debes Iniciar Sesión antes');
+    }
+} else {
+    exit('Recurso denegado');
+}
--- Backend/core/models/datos-familiares.php
+++ Backend/core/models/datos-familiares.php
@@ -6,9 +6,253 @@ class DatosFamiliares extends Validator
     private $Nombres = null;
     private $Apellidos = null;
     private $Fecha = null;
+    private $Id_parentesco = null;
     private $Parentesco = null;
+    private $Id_colaborador = null;
     private $Colaborador = null;
     private $Genero = null;
     private $Telefono = null;
     private $Estado = null;
+
+    /**
+     * Get the value of Id
+     */ 
+    public function getId()
+    {
+        return $this->Id;
+    }
+
+    /**
+     * Set the value of Id
+     *
+     * @return  self
+     */ 
+    public function setId($Id)
+    {
+        $this->Id = $Id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Nombres
+     */ 
+    public function getNombres()
+    {
+        return $this->Nombres;
+    }
+
+    /**
+     * Set the value of Nombres
+     *
+     * @return  self
+     */ 
+    public function setNombres($Nombres)
+    {
+        $this->Nombres = $Nombres;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Apellidos
+     */ 
+    public function getApellidos()
+    {
+        return $this->Apellidos;
+    }
+
+    /**
+     * Set the value of Apellidos
+     *
+     * @return  self
+     */ 
+    public function setApellidos($Apellidos)
+    {
+        $this->Apellidos = $Apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Fecha
+     */ 
+    public function getFecha()
+    {
+        return $this->Fecha;
+    }
+
+    /**
+     * Set the value of Fecha
+     *
+     * @return  self
+     */ 
+    public function setFecha($Fecha)
+    {
+        $this->Fecha = $Fecha;
+
+        return $this;
+    }
+
+
+
+    /**
+     * Get the value of Id_parentesco
+     */ 
+    public function getId_parentesco()
+    {
+        return $this->Id_parentesco;
+    }
+
+    /**
+     * Set the value of Id_parentesco
+     *
+     * @return  self
+     */ 
+    public function setId_parentesco($Id_parentesco)
+    {
+        $this->Id_parentesco = $Id_parentesco;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Parentesco
+     */ 
+    public function getParentesco()
+    {
+        return $this->Parentesco;
+    }
+
+    /**
+     * Set the value of Parentesco
+     *
+     * @return  self
+     */ 
+    public function setParentesco($Parentesco)
+    {
+        $this->Parentesco = $Parentesco;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Id_colaborador
+     */ 
+    public function getId_colaborador()
+    {
+        return $this->Id_colaborador;
+    }
+
+    /**
+     * Set the value of Id_colaborador
+     *
+     * @return  self
+     */ 
+    public function setId_colaborador($Id_colaborador)
+    {
+        $this->Id_colaborador = $Id_colaborador;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Colaborador
+     */ 
+    public function getColaborador()
+    {
+        return $this->Colaborador;
+    }
+
+    /**
+     * Set the value of Colaborador
+     *
+     * @return  self
+     */ 
+    public function setColaborador($Colaborador)
+    {
+        $this->Colaborador = $Colaborador;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Genero
+     */ 
+    public function getGenero()
+    {
+        return $this->Genero;
+    }
+
+    /**
+     * Set the value of Genero
+     *
+     * @return  self
+     */ 
+    public function setGenero($Genero)
+    {
+        $this->Genero = $Genero;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Telefono
+     */ 
+    public function getTelefono()
+    {
+        return $this->Telefono;
+    }
+
+    /**
+     * Set the value of Telefono
+     *
+     * @return  self
+     */ 
+    public function setTelefono($Telefono)
+    {
+        $this->Telefono = $Telefono;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Estado
+     */ 
+    public function getEstado()
+    {
+        return $this->Estado;
+    }
+
+    /**
+     * Set the value of Estado
+     *
+     * @return  self
+     */ 
+    public function setEstado($Estado)
+    {
+        $this->Estado = $Estado;
+
+        return $this;
+    }
+
+    public function readDatosFamiliares()
+    {
+        $sql = 'SELECT d.Nombres, d.Apellidos, d.Fecha_nacimiento, d.Dependiente, p.Parentesco, c.Nombres as colaborador, d.Genero, d.Numero_telefono, d.Estado
+        FROM datosfamiliares as d
+        INNER JOIN parentesco as p on d.Id_parentesco = p.Id_parentesco
+        INNER JOIN colaborador as c on d.Id_Colaborador = c.Id_Colaborador
+        ORDER BY d.Nombres';
+        $params = array(null);
+        return Database::getRows($sql, $params);
+    }
+
+    public function createDatosFamiliares()
+    {
+        $sql = 'insert into datos_familiares (Nombres, Apellidos, Fecha_nacimiento, Dependiente, Id_parentesco, Id_colaborador, Genero, Numero_telefono, Estado) VALUES (?,?,?,?,?,?,?,?,?)';
+        $params = array($this->Nombres, $this->Apellidos, $this->Fecha_nacimiento, $this->Dependiente, $this->Id_parentesco, $this->Id_colaborador, $this->Genero, $this->Numero_telefono, $this->Estado);
+        return Database::executeRow($sql, $params);
+    }
 }
+
+?>
